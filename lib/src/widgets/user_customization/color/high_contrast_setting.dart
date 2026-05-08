0820c9d4235b4cf126e3e3ba1d5cc15a6b6ea47e
empathictech <105748722+empathictech@users.noreply.github.com>
2026-05-07 20:48:43 -0400

    updated colors

--- lib/src/widgets/user_customization/color/high_contrast_setting.dart
+++ lib/src/widgets/user_customization/color/high_contrast_setting.dart
@@ -7,118 +7,6 @@ import '../../../../empathetech_flutter_ui.dart';
 
 import 'package:flutter/material.dart';
 
-/// High contrast Empath green ==
-/// 0xFF00FFB4
-const int hceGreenHex = 0xFF00FFB4;
-
-/// High contrast Empath green ==
-/// 0xFF00FFB4
-const Color hceGreen = Color(hceGreenHex);
-
-/// High contrast Empath gold ==
-/// 0xFFFFB400
-const int hceGoldHex = 0xFFFFB400;
-
-/// High contrast Empath gold ==
-/// 0xFFFFB400
-const Color hceGold = Color(0xFFFFB400);
-
-/// High contrast Empath purple ==
-/// 0xFFB400FF
-const int hcePurpleHex = 0xFFB400FF;
-
-/// High contrast Empath purple ==
-/// 0xFFB400FF
-const Color hcePurple = Color(0xFFB400FF);
-
-/// 0xFFFF6700
-const int safetyOrangeHex = 0xFFFF6700;
-
-/// 0xFFFF6700
-const Color safetyOrange = Color(safetyOrangeHex);
-
-/// Custom [ColorScheme.highContrastDark]
-const ColorScheme ezHighContrastDark = ColorScheme(
-  brightness: Brightness.dark,
-  // Primary
-  primary: hceGreen,
-  onPrimary: Colors.black,
-  primaryContainer: Colors.white,
-  onPrimaryContainer: Colors.black,
-
-  // Secondary
-  secondary: hceGold,
-  onSecondary: Colors.black,
-  secondaryContainer: Colors.white,
-  onSecondaryContainer: Colors.black,
-
-  // Tertiary
-  tertiary: hcePurple,
-  onTertiary: Colors.white,
-  tertiaryContainer: Colors.white,
-  onTertiaryContainer: Colors.black,
-
-  // Error
-  error: safetyOrange,
-  onError: Colors.white,
-  errorContainer: Colors.white,
-  onErrorContainer: Colors.black,
-
-  // Surface
-  surfaceContainer: Colors.black,
-  surfaceDim: darkSurfaceContainer,
-  surface: darkSurfaceDim,
-  onSurface: Colors.white,
-
-  // Misc
-  outline: darkOutline,
-  outlineVariant: darkOutline,
-  scrim: Colors.black,
-  shadow: Colors.transparent,
-  surfaceTint: Colors.transparent,
-);
-
-/// Custom [ColorScheme.highContrastLight]
-const ColorScheme ezHighContrastLight = ColorScheme(
-  brightness: Brightness.light,
-  // Primary
-  primary: hcePurple,
-  onPrimary: Colors.white,
-  primaryContainer: Colors.black,
-  onPrimaryContainer: Colors.white,
-
-  // Secondary
-  secondary: hceGold,
-  onSecondary: Colors.black,
-  secondaryContainer: Colors.black,
-  onSecondaryContainer: Colors.white,
-
-  // Tertiary
-  tertiary: hceGreen,
-  onTertiary: Colors.black,
-  tertiaryContainer: Colors.black,
-  onTertiaryContainer: Colors.white,
-
-  // Error
-  error: safetyOrange,
-  onError: Colors.white,
-  errorContainer: Colors.black,
-  onErrorContainer: Colors.white,
-
-  // Surface
-  surfaceContainer: lightSurfaceContainer,
-  surfaceDim: lightSurfaceDim,
-  surface: Colors.white,
-  onSurface: Colors.black,
-
-  // Misc
-  outline: lightOutline,
-  outlineVariant: lightOutline,
-  scrim: Colors.white,
-  shadow: Colors.transparent,
-  surfaceTint: Colors.transparent,
-);
-
 class EzHighContrastColorsSetting extends StatelessWidget {
   /// [ThemeData.colorScheme] for [Brightness.dark]
   final ColorScheme dark;
@@ -169,3 +57,97 @@ class EzHighContrastColorsSetting extends StatelessWidget {
         label: EzConfig.l10n.csHighContrast,
       );
 }
+
+/// 0xFF00FFB4
+const Color hceGreen = Color(0xFF00FFB4);
+
+/// 0xFFFFB400
+const Color hceGold = Color(0xFFFFB400);
+
+/// 0xFFB400FF
+const Color hcePurple = Color(0xFFB400FF);
+
+/// 0xFFFF6700
+const Color hcError = Color(0xFFFF6700);
+
+/// Custom [ColorScheme.highContrastDark]
+const ColorScheme ezHighContrastDark = ColorScheme(
+  brightness: Brightness.dark,
+  // Primary
+  primary: hceGreen,
+  onPrimary: Colors.black,
+  primaryContainer: halfWhite,
+  onPrimaryContainer: Colors.black,
+
+  // Secondary
+  secondary: hceGold,
+  onSecondary: Colors.black,
+  secondaryContainer: halfWhite,
+  onSecondaryContainer: Colors.black,
+
+  // Tertiary
+  tertiary: hcePurple,
+  onTertiary: Colors.white,
+  tertiaryContainer: halfWhite,
+  onTertiaryContainer: Colors.black,
+
+  // Error
+  error: hcError,
+  onError: Colors.white,
+  errorContainer: halfWhite,
+  onErrorContainer: Colors.black,
+
+  // Surface
+  surfaceContainer: Colors.black,
+  surfaceDim: darkSurfaceContainer,
+  surface: darkSurfaceDim,
+  onSurface: Colors.white,
+
+  // Misc
+  outline: Colors.white,
+  outlineVariant: halfWhite,
+  scrim: Colors.black,
+  shadow: Colors.transparent,
+  surfaceTint: Colors.transparent,
+);
+
+/// Custom [ColorScheme.highContrastLight]
+const ColorScheme ezHighContrastLight = ColorScheme(
+  brightness: Brightness.light,
+  // Primary
+  primary: hcePurple,
+  onPrimary: Colors.white,
+  primaryContainer: halfBlack,
+  onPrimaryContainer: Colors.white,
+
+  // Secondary
+  secondary: hceGold,
+  onSecondary: Colors.black,
+  secondaryContainer: halfBlack,
+  onSecondaryContainer: Colors.white,
+
+  // Tertiary
+  tertiary: hceGreen,
+  onTertiary: Colors.black,
+  tertiaryContainer: halfBlack,
+  onTertiaryContainer: Colors.white,
+
+  // Error
+  error: hcError,
+  onError: Colors.white,
+  errorContainer: halfBlack,
+  onErrorContainer: Colors.white,
+
+  // Surface
+  surfaceContainer: lightSurfaceContainer,
+  surfaceDim: lightSurfaceDim,
+  surface: Colors.white,
+  onSurface: Colors.black,
+
+  // Misc
+  outline: Colors.black,
+  outlineVariant: halfBlack,
+  scrim: Colors.white,
+  shadow: Colors.transparent,
+  surfaceTint: Colors.transparent,
+);
