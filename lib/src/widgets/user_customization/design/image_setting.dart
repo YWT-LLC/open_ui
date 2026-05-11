b87baaefaaf43ab7344aa9af2f417072a3374e1e
empathictech <105748722+empathictech@users.noreply.github.com>
2026-05-11 07:08:34 -0400

    updated button padding

--- lib/src/widgets/user_customization/design/image_setting.dart
+++ lib/src/widgets/user_customization/design/image_setting.dart
@@ -600,6 +600,7 @@ class _ImageSettingState extends State<EzImageSetting> {
                 EzTextButton(
                   onPressed: () => Navigator.of(fitContext).pop(null),
                   text: EzConfig.l10n.gCancel,
+                  textPadding: true,
                   textStyle: EzConfig.styles.bodyLarge,
                   textAlign: TextAlign.center,
                 ),
@@ -614,6 +615,7 @@ class _ImageSettingState extends State<EzImageSetting> {
                     }
                   },
                   text: currFit == null ? EzConfig.l10n.gSkip : EzConfig.l10n.gApply,
+                  textPadding: true,
                   textStyle: EzConfig.styles.bodyLarge?.copyWith(color: EzConfig.colors.primary),
                   textAlign: TextAlign.center,
                 ),
