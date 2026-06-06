4b0b1aba4a99a1863509d984c482775d4ba55794
empathictech <105748722+empathictech@users.noreply.github.com>
2026-06-06 17:28:02 -0400

    cleanup

--- example/lib/screens/settings.dart
+++ example/lib/screens/settings.dart
@@ -16,100 +16,100 @@ class SettingsHubScreen extends StatelessWidget {
   const SettingsHubScreen({super.key, this.targetPass});
 
   @override
-  Widget build(BuildContext context) => Consumer<EZCProvider>(
-        builder: (_, EZCProvider config, __) => OpenUIScaffold(
-          EzScreen(EzSettingsHub(
-            pages: <EzSettingsSection>[
-              // Global //
+  Widget build(BuildContext context) {
+    return Consumer<EzCP>(
+      builder: (_, EzCP config, __) => OpenUIScaffold(
+        EzScreen(EzSettingsHub(
+          pages: <EzSettingsSection>[
+            // Global //
 
-              EzSettingsSection(
-                position: 0,
-                title: config.l10n.gGlobal,
-                icon: EzIcon(
-                  config.onMobile
-                      ? config.platform == TargetPlatform.iOS
-                          ? Icons.phone_iphone
-                          : Icons.phone_android
-                      : Icons.computer,
-                  semanticLabel: config.l10n.gGlobal,
-                ),
-                subSettings: <EzSubSetting>[],
-                fromStorage: () => EzSubSetting.blank,
-                build: (_) => const EzGlobalSettings(),
+            EzSettingsSection(
+              position: 0,
+              title: config.l10n.gGlobal,
+              icon: EzIcon(
+                EzCM.onMobile
+                    ? EzCM.platform == TargetPlatform.iOS
+                        ? Icons.phone_iphone
+                        : Icons.phone_android
+                    : Icons.computer,
+                semanticLabel: config.l10n.gGlobal,
               ),
+              subSettings: <EzSubSetting>[],
+              fromStorage: () => EzSubSetting.blank,
+              build: (_) => const EzGlobalSettings(),
+            ),
 
-              // Color //
+            // Color //
 
-              EzSettingsSection(
-                position: 1,
-                title: config.l10n.gColor,
-                icon: EzIcon(
-                  Icons.palette,
-                  semanticLabel: config.l10n.gColor,
-                ),
-                subSettings: <EzSubSetting>[
-                  EzSubSetting.qckColor,
-                  EzSubSetting.advColor,
-                ],
-                fromStorage: () => EZCManager.get(advancedColorsKey) == true
-                    ? EzSubSetting.advColor
-                    : EzSubSetting.qckColor,
-                build: (EzSubSetting subSec) => EzColorSettings(target: subSec),
+            EzSettingsSection(
+              position: 1,
+              title: config.l10n.gColor,
+              icon: EzIcon(
+                Icons.palette,
+                semanticLabel: config.l10n.gColor,
               ),
+              subSettings: <EzSubSetting>[
+                EzSubSetting.qckColor,
+                EzSubSetting.advColor,
+              ],
+              fromStorage: () => EzCM.get(advancedColorsKey) == true
+                  ? EzSubSetting.advColor
+                  : EzSubSetting.qckColor,
+              build: (EzSubSetting subSec) => EzColorSettings(target: subSec),
+            ),
 
-              // Design //
+            // Design //
 
-              EzSettingsSection(
-                position: 2,
-                title: config.l10n.gDesign,
-                icon: EzIcon(
-                  Icons.design_services,
-                  semanticLabel: config.l10n.gDesign,
-                ),
-                subSettings: <EzSubSetting>[
-                  EzSubSetting.butDesign,
-                  EzSubSetting.pagDesign,
-                ],
-                fromStorage: () => EZCManager.get(pageTabKey) == true
-                    ? EzSubSetting.pagDesign
-                    : EzSubSetting.butDesign,
-                build: (EzSubSetting subSec) => EzDesignSettings(target: subSec),
+            EzSettingsSection(
+              position: 2,
+              title: config.l10n.gDesign,
+              icon: EzIcon(
+                Icons.design_services,
+                semanticLabel: config.l10n.gDesign,
               ),
+              subSettings: <EzSubSetting>[
+                EzSubSetting.butDesign,
+                EzSubSetting.pagDesign,
+              ],
+              fromStorage: () =>
+                  EzCM.get(pageTabKey) == true ? EzSubSetting.pagDesign : EzSubSetting.butDesign,
+              build: (EzSubSetting subSec) => EzDesignSettings(target: subSec),
+            ),
 
-              // Text //
+            // Text //
 
-              EzSettingsSection(
-                position: 3,
-                title: config.l10n.gText,
-                icon: EzIcon(
-                  Icons.text_format,
-                  semanticLabel: config.l10n.gText,
-                ),
-                subSettings: <EzSubSetting>[
-                  EzSubSetting.qckText,
-                  EzSubSetting.advText,
-                ],
-                fromStorage: () => EZCManager.get(advancedTextKey) == true
-                    ? EzSubSetting.advText
-                    : EzSubSetting.qckText,
-                build: (EzSubSetting subSec) => EzTextSettings(target: subSec),
+            EzSettingsSection(
+              position: 3,
+              title: config.l10n.gText,
+              icon: EzIcon(
+                Icons.text_format,
+                semanticLabel: config.l10n.gText,
               ),
-            ],
-            target: targetPass,
-          )),
-          title: config.l10n.gSettings,
-          showSettings: false,
-          fabs: <Widget>[
-            // Rebuild (conditional)
-            if (config.needsRebuild) ...<Widget>[
-              config.spacer,
-              const EzRebuildFAB(),
-            ],
-
-            // Save/upload config
-            config.spacer,
-            const EzConfigFAB(),
+              subSettings: <EzSubSetting>[
+                EzSubSetting.qckText,
+                EzSubSetting.advText,
+              ],
+              fromStorage: () =>
+                  EzCM.get(advancedTextKey) == true ? EzSubSetting.advText : EzSubSetting.qckText,
+              build: (EzSubSetting subSec) => EzTextSettings(target: subSec),
+            ),
           ],
-        ),
-      );
+          target: targetPass,
+        )),
+        title: config.l10n.gSettings,
+        showSettings: false,
+        fabs: <Widget>[
+          // Rebuild (conditional)
+          if (config.needsRebuild) ...<Widget>[
+            config.spacer,
+            const EzRebuildFAB(),
+          ],
+
+          // Save/upload config
+          config.spacer,
+          const EzConfigFAB(),
+        ],
+      ),
+    );
+  }
 }
