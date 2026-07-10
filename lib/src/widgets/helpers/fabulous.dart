912ea327a4db996cb5a7ac23e831ffcaa640dacd
empathictech <105748722+empathictech@users.noreply.github.com>
2026-07-10 10:03:48 -0400

    added icons

--- lib/src/widgets/helpers/fabulous.dart
+++ lib/src/widgets/helpers/fabulous.dart
@@ -47,11 +47,13 @@ class EzConfigFAB extends StatelessWidget {
           EzMenuButton(
             config,
             label: config.ezL10n.ssSaveConfig,
+            icon: EzIcon(config, Icons.download),
             onPressed: () => EzCM.saveConfig(config, context: context, skip: skip),
           ),
           EzMenuButton(
             config,
             label: config.ezL10n.ssLoadConfig,
+            icon: EzIcon(config, Icons.upload),
             onPressed: () => ezConfigLoader(config, context: context),
           ),
         ],
