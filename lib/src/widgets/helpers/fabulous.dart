0678c7031745873822c14bbbef41836263abae7c
empathictech <105748722+empathictech@users.noreply.github.com>
2026-07-19 19:18:20 -0400

    fixed year

--- lib/src/widgets/helpers/fabulous.dart
+++ lib/src/widgets/helpers/fabulous.dart
@@ -1,5 +1,5 @@
 /* empathetech_flutter_ui
- * Copyright (c) 2026 Empathetech LLC. All rights reserved.
+ * Copyright (c) 2022 Empathetech LLC. All rights reserved.
  * See LICENSE for distribution and usage details.
  */
 
