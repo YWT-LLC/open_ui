46041f65853b9b2c151d1b9609b3f462f142bb54
empathictech <105748722+empathictech@users.noreply.github.com>
2026-08-29 08:42:30 -0400

    version bump

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -14,7 +14,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "net.empathetech.open_ui"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
     
     compileOptions {
