3ce2d66e3678b66a11b87b7439d04c224c479d6f
empathictech <105748722+empathictech@users.noreply.github.com>
2026-09-20 19:26:55 -0400

    swap uses config now

--- lib/src/widgets/responsive_design/swap_widget.dart
+++ lib/src/widgets/responsive_design/swap_widget.dart
@@ -8,6 +8,9 @@ import '../../../open_ui.dart';
 import 'package:flutter/material.dart';
 
 class EzSwapWidget extends StatelessWidget {
+  /// EzConfig Provider
+  final EzCP config;
+
   /// Which [ScreenSize] the Widget should respond to
   final ScreenSize breakpoint;
 
@@ -17,19 +20,30 @@ class EzSwapWidget extends StatelessWidget {
   /// Displayed when the context's [ScreenSize] <= [breakpoint]
   final Widget restricted;
 
+  /// Whether the [Widget] should be animated via [EzAnimSwitch]
+  final bool animate;
+
+  /// [EzAnimSwitch.mod] passthrough
+  final double mod;
+
   /// [ScreenSize] > [breakpoint] => [expanded]
   /// [ScreenSize] <= [breakpoint] => [restricted]
   /// [ScreenSize] is not in the Widget tree => [restricted]
-  const EzSwapWidget({
+  const EzSwapWidget(
+    this.config, {
     super.key,
+    this.animate = false,
+    this.mod = 1.0,
     this.breakpoint = ScreenSize.small,
     required this.restricted,
     required this.expanded,
   });
 
+  Widget _core(ScreenSize? size) =>
+      (size == null || size.order <= breakpoint.order) ? restricted : expanded;
+
   @override
-  Widget build(BuildContext context) {
-    final ScreenSize? size = EzScreenSize.of(context)?.screenSize;
-    return (size == null || size.order <= breakpoint.order) ? restricted : expanded;
-  }
+  Widget build(BuildContext context) => animate
+      ? EzAnimSwitch(config, mod: mod, child: _core(EzScreenSize.of(context)?.screenSize))
+      : _core(EzScreenSize.of(context)?.screenSize);
 }
